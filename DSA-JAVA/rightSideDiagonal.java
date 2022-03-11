ecd83d005470b3fe81843f4bba17aa1838de1456
Shivanshu Singh <ss3591419@gmail.com>
2022-03-11 19:52:29 +0530

    Patterns

---
+++ DSA-JAVA/rightSideDiagonal.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class rightSideDiagonal {
+    public static void main(String[] args){
+
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt();
+        for(int i = 1;i<=num;i++){
+            for(int j = 1;j<=num;j++){
+                if(i+j==num+1){
+                    System.out.print("*\t");
+                }
+                else{
+                    System.out.print("\t");
+                }
+
+            }
+            System.out.println();
+        }
+
+    }
+}
